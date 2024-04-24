b46950031d546339be550712205e152da9ae13b2
lharo <lharo@insdosl.com>
2024-04-24 09:39:48 +0200

    working with hugging face

--- llm_service/app.py
+++ llm_service/app.py
@@ -3,15 +3,18 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import os
 from fastapi.middleware.cors import CORSMiddleware
-import openai
+# import openai
+from langchain_community.embeddings import HuggingFaceInstructEmbeddings
+from langchain_community.chat_models import ChatHuggingFace 
+from langchain_community.llms import HuggingFaceEndpoint, HuggingFaceHub
+
 from langchain.prompts import PromptTemplate
 import logging
 from dotenv import find_dotenv, load_dotenv
-from langchain.embeddings.openai import OpenAIEmbeddings
+# from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores.pgvector import PGVector
-from langchain.chat_models import ChatOpenAI
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores.pgvector import PGVector
+# from langchain.chat_models import ChatOpenAI
+# from langchain.embeddings import OpenAIEmbeddings
 from langchain.schema import (
     AIMessage,
     HumanMessage,
@@ -29,7 +32,7 @@ ROLE_CLASS_MAP = {
 }
 
 load_dotenv(find_dotenv())
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# openai.api_key = os.getenv("OPENAI_API_KEY")
 CONNECTION_STRING = "postgresql+psycopg2://admin:admin@postgres:5432/vectordb"
 COLLECTION_NAME="vectordb"
 
@@ -43,8 +46,24 @@ class Message(BaseModel):
 class Conversation(BaseModel):
     conversation: List[Message]
 
-embeddings = OpenAIEmbeddings()
-chat = ChatOpenAI(temperature=0)
+# embeddings = OpenAIEmbeddings()
+embeddings = HuggingFaceInstructEmbeddings(
+    model_name="hkunlp/instructor-xl",
+    model_kwargs={'device': 'cpu'},
+    encode_kwargs={'normalize_embeddings': True}
+)
+llm = HuggingFaceHub(
+    repo_id="google/gemma-7b",
+    model_kwargs={
+    "max_new_tokens": 2048,
+    "top_k": 30,
+    "temperature": 1,
+    "repetition_penalty": 1.03,
+    "max_length": 16384,
+})
+
+chat = ChatHuggingFace(llm=llm)
+
 store = PGVector(
     collection_name=COLLECTION_NAME,
     connection_string=CONNECTION_STRING,
